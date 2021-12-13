7702f1dd9befc5030385d1307bd8bdae1a8caa3e
pauline rouger <paulinerouge@hotmail.com>
2021-12-13 10:14:25 +0800

    Added validation review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
+  validates :content, length: { minimum: 20 }
   validates :rating, inclusion: { in: 0..5 }
   validates :rating, numericality: { only_integer: true }
 end
